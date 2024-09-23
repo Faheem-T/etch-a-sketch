9f51a87360a71d66edb7805d2cfc438f7005e443
Faheem <faheemmbasheer@gmail.com>
2024-09-23 12:24:38 +0530

    create the 16 x 16 grid

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const container = document.querySelector("#container");
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement("div");
+    row.setAttribute("class", "row");
+    for (let j = 0; j < 16; j++) {
+        const box = document.createElement("div");
+        box.setAttribute("class", "box");
+        row.appendChild(box);
+    }
+    container.appendChild(row);
+}
