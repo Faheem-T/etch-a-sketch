d7df3e66df51ebadece44c66e86cc09da345abdf
Faheem <faheemmbasheer@gmail.com>
2024-09-23 15:49:29 +0530

    implement button to change grid size

--- scripts.js
+++ scripts.js
@@ -1,21 +1,40 @@
-const container = document.querySelector("#container");
+// creates a grid of n x n brown boxes
+function createGrid(n){
+    const container = document.querySelector("#container");
+    // clearing the container for next render
+    container.innerHTML = "";
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.setAttribute("class", "row");
-    container.appendChild(row);
-    for (let j = 0; j < 16; j++) {
-        const box = document.createElement("div");
-        box.setAttribute("class", "box");
-        row.appendChild(box);
-
-        // Changes the color of the box first to white
-        // then to black after 1.5 seconds
-        box.addEventListener("mouseenter", function(){
-            this.style.backgroundColor = "white";
-            setTimeout(() => {
-                this.style.backgroundColor = "black";
-            }, 1.5 * 1000);
-        });
+    for (let i = 0; i < n; i++) {
+        const row = document.createElement("div");
+        row.setAttribute("class", "row");
+        container.appendChild(row);
+        for (let j = 0; j < n; j++) {
+            const box = document.createElement("div");
+            box.setAttribute("class", "box");
+            row.appendChild(box);
+    
+            // Changes the color of the box first to white
+            // then to black after 1.5 seconds
+            box.addEventListener("mouseenter", function(){
+                this.style.backgroundColor = "white";
+                setTimeout(() => {
+                    this.style.backgroundColor = "black";
+                }, 1.5 * 1000);
+            });
+        }
     }
 }
+
+// default first render is 16 x 16
+createGrid(16);
+
+// button that changes the dimensions of the grid
+const changeButton = document.querySelector("#changeGridButton");
+changeButton.addEventListener("click", function(){
+    let number = prompt("Enter grid size:");
+    number = +number;
+    if (number > 0 && number <= 100) {
+        console.log(number + ":" + typeof(number));
+        createGrid(number);
+    }
+});
